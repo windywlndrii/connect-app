d2f93cefc7bb9bd4977c5d07ac877264cd7ed587
drianali <maulanaalisaputra27@gmail.com>
2025-05-14 11:32:25 +0800

    add content :user page

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,19 +1,112 @@
-import { IconLogout2, IconUser, IconUserCog } from "@tabler/icons-react";
+import {
+  IconLogout2,
+  IconUser,
+  IconUserBolt,
+  IconUserCog,
+} from "@tabler/icons-react";
 
 export default function UserPages() {
-    return (
-        <div id="container" className="flex h-[100vh]">
-           <section id="navigation" className="bg-blue-500 p-10 w-[200px]">
-           <h1 className="text-3xl font-bold text-center">Connect</h1>
-           <div className="flex flex-col gap-2 mt-4">
-            <button className="flex gap-2"> <IconUser />  Users</button>
-            <button className="flex gap-2"> <IconUserCog /> Hak akses</button>
-            <button className="flex gap-2"> <IconLogout2 /> Logout</button>
-           </div>
-           </section>
-           <section id = "content">
-            <input type="search" placeholder="Cari User" className="p-5 w-500"></input>
-           </section>
+  return (
+    <div id="container" className="flex h-[100vh] text-black">
+      <section
+        id="navigation"
+        className="bg-white p-10 w-[220px] border-r flex flex-col"
+      >
+        <h1 className="text-3xl font-bold text-center">Connect</h1>
+        <div className="flex flex-col gap-3 mt-15">
+          <button className="p-2.5 text-left text-base cursor-pointer flex items-center gap-2 rounded-lg font-bold">
+            {" "}
+            <IconUser /> Users
+          </button>
+          <button className="p-2.5 text-left text-base cursor-pointer flex items-center gap-2 rounded-lg font-bold">
+            {" "}
+            <IconUserCog /> Hak akses
+          </button>
+          <button className="p-2.5 text-left text-base cursor-pointer flex items-center gap-2 rounded-lg font-bold">
+            {" "}
+            <IconLogout2 /> Logout
+          </button>
         </div>
-    );
+      </section>
+      <section
+        id="content"
+        className="bg-white w-[85%] flex-1 p-[30px] overflow-y-auto"
+      >
+        <input
+          type="search"
+          placeholder="Cari User"
+          className="w-full p-3 border rounded-lg mb-7 text-base"
+        ></input>
+        <div className="flex flex-col gap-4">
+          <div className="border rounded-lg p-2 flex justify-between items-center">
+            <div>
+              <h2 className="text-[18px] font-bold mb-[1px]">
+                Rahmat Saudi Al Fathir As
+              </h2>
+              <p className="text-base text-black mb-2">
+                rahmatsaudi@universitasmulia.ac.id
+              </p>
+              <span className="bg-black text-white text-[11px] px-[9px] py-[5px] rounded mr-[6px]">
+                Admin
+              </span>
+              <span className="bg-black text-white text-[11px] px-[9px] py-[5px] rounded">
+                Employee
+              </span>
+            </div>
+            <span className="bold-text status text-[16px] font-bold">
+              Aktif
+            </span>
+          </div>
+
+          <div className="border rounded-lg p-2 flex justify-between items-center">
+            <div>
+              <h2 className="text-[18px] font-bold mb-[1px]">Lintang</h2>
+              <p className="text-base text-black mb-2">
+                lintang@universitasmulia.ac.id
+              </p>
+              <span className="bg-black text-white text-[11px] px-[9px] py-[5px] rounded">
+                Employee
+              </span>
+            </div>
+            <span className="bold-text status text-[16px] font-bold">
+              Aktif
+            </span>
+          </div>
+
+          <div className="border rounded-lg p-2 flex justify-between items-center">
+            <div>
+              <h2 className="text-[18px] font-bold mb-[1px]">Shafira</h2>
+              <p className="text-base text-black mb-2">
+                shafira@universitasmulia.ac.id
+              </p>
+              <span className="bg-black text-white text-[11px] px-[9px] py-[5px] rounded">
+                Employee
+              </span>
+            </div>
+            <span className="bold-text status text-[16px] font-bold">
+              Aktif
+            </span>
+          </div>
+
+          <div className="border rounded-lg p-2 flex justify-between items-center">
+            <div>
+              <h2 className="text-[18px] font-bold mb-[1px]">Lebah Ganteng</h2>
+              <p className="text-base text-black mb-2">
+                lebahganteng@universitasmulia.ac.id
+              </p>
+              <span className="bg-black text-white text-[11px] px-[9px] py-[5px] rounded">
+                Employee
+              </span>
+            </div>
+            <span className="bold-text status text-[16px] font-bold">
+              Suspended
+            </span>
+          </div>
+        </div>
+      </section>
+      <button className="fixed right-5 bottom-5 w-[42px] h-[42px] rounded-lg text-[26px] bg-[#e0e0e0] cursor-pointer">
+        +
+      </button>
+    </div>
+  );
 }
